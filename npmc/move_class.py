e08bb87f9202ed00f6d11d9dd0db9de048a77433
Zachary LaFaver <127230006+zlafa24@users.noreply.github.com>
2024-08-08 13:30:16 -0400

    Update move_class.py

--- npmc/move_class.py
+++ npmc/move_class.py
@@ -66,6 +66,7 @@ class MDmove(Move):
         self.simulation.lmp.command('velocity sulfur set 0.0 0.0 0.0')
         self.simulation.lmp.command("fix fxfrcS sulfur setforce 0.0 0.0 0.0")
         self.simulation.lmp.command('fix staystill sulfur spring/self 30000.0')
+        self.simulation.lmp.command(f'velocity adsorbate create {self.temp} dist gaussian sum no mom yes rot yes') #establishes random velocities to the ligand chains from a gaussian distribution and resets linear and angular momentum
         self.simulation.lmp.command(f'fix mynvt adsorbate nvt temp {self.temp} {self.temp} $({100.0}*dt)')
         #self.simulation.lmp.command(f'timestep {self.timestep}')
         self.simulation.lmp.command(f'run {self.numsteps}')
